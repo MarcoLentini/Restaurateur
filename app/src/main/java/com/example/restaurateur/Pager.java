9534238729cd90e42c6030242de36f49f24540dc
Cosmin Daniel <streetking46cosmin@gmail.com>
2019-04-08 16:38:49 +0200

    Tabs

---
+++ app/src/main/java/com/example/restaurateur/Pager.java
@@ -0,0 +1,44 @@
+package com.example.restaurateur;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+public class Pager extends FragmentStatePagerAdapter {
+
+    //integer to count number of tabs
+    int tabCount;
+
+    //Constructor to the class
+    public Pager(FragmentManager fm, int tabCount) {
+        super(fm);
+        //Initializing tab count
+        this.tabCount= tabCount;
+    }
+
+    //Overriding method getItem
+    @Override
+    public Fragment getItem(int position) {
+        //Returning the current tabs
+        switch (position) {
+            case 0:
+                TabReservations tab1 = new TabReservations();
+                return tab1;
+            case 1:
+                TabActiveOffers tab2 = new TabActiveOffers();
+                return tab2;
+            case 2:
+                TabDisabledOffers tab3 = new TabDisabledOffers();
+                return tab3;
+            default:
+                return null;
+        }
+    }
+
+    //Overriden method getCount to get the number of tabs
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
+
