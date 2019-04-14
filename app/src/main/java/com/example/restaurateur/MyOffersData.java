321631e8e8380aebb41056f905c0ab30f75f8970
Cosmin Daniel <streetking46cosmin@gmail.com>
2019-04-14 20:38:03 +0200

    Added MyOffersData.java

---
+++ app/src/main/java/com/example/restaurateur/MyOffersData.java
@@ -0,0 +1,17 @@
+package com.example.restaurateur;
+
+public class MyOffersData {
+    static Integer[] id = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11,
+            12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+
+    static String[] offerName = { "Pizza Margherita", "Pizza al tonno", "Pizza tricolore",
+            "Pizza con prosiutto cotto, friarielli, salsiccia e tanta gorgonzola", "Pizza quattro formaggi",
+            "Hamburger classico", "Cheeseburger", "Cheeseburger con bacon ed emmental" , "Pasta al forno",
+            "Lasagne al pesto", "Lasagne alla bolognese", "Pasta al pomodoro e funghi", "Pasta al pesto",
+            "Zucchine", "Patatine fritte", "Hamburger e patatine fritte", "Bistecca di pollo impanata",
+            "Zucchine grigliate", "Peperoni grigliati", "Pizza al prosciutto crudo e gorgonzola", "Vitello tonnato",
+            "Insalata", "Ketchup", "Maionese", "Coca-cola in lattine da 33cl" };
+
+    static Integer[] price = {4, 5, 8, 6, 9, 6, 7, 8, 9, 10, 11,
+            12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25};
+}
