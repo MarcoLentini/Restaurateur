cf88c2d63ee9017d284fdbd812dd896157462011
echojiashu <echojiashu631@163.com>
2019-04-18 15:50:46 +0200

    chang some buttons styles

--- app/src/main/java/com/example/restaurateur/Reservation/InProgressReservationsListAdapter.java
+++ app/src/main/java/com/example/restaurateur/Reservation/InProgressReservationsListAdapter.java
@@ -2,7 +2,11 @@ package com.example.restaurateur.Reservation;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.transition.Fade;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +24,7 @@ import java.util.HashMap;
 public class InProgressReservationsListAdapter extends RecyclerView.Adapter<InProgressReservationsListAdapter.InProgressReservationViewHolder> {
 
     private Context context;
+   // private FragmentActivity fragmentActivity;
     private ArrayList<ReservationModel> pendingDataSet;
     private ArrayList<ReservationModel> inProgressDataSet;
     private ArrayList<ReservationModel> finishedDataSet;
@@ -27,8 +32,9 @@ public class InProgressReservationsListAdapter extends RecyclerView.Adapter<InPr
     private HashMap<Integer, OfferModel> offersData;
 
     public InProgressReservationsListAdapter(Context context, ArrayList<ReservationModel> pendingData, ArrayList<ReservationModel> inProgressData,
-                                             ArrayList<ReservationModel> finishedData, HashMap<Integer, OfferModel> offersData) {
+                                             ArrayList<ReservationModel> finishedData, HashMap<Integer, OfferModel> offersData) {//,FragmentActivity fragmentActivity
         this.context = context;
+       // this.fragmentActivity = fragmentActivity;
         this.mInflater = LayoutInflater.from(context);
         this.pendingDataSet = pendingData;
         this.inProgressDataSet = inProgressData;
@@ -41,19 +47,21 @@ public class InProgressReservationsListAdapter extends RecyclerView.Adapter<InPr
     public InProgressReservationViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = mInflater.inflate(R.layout.in_progress_reservation_cardview, parent, false);
 
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent myIntent = new Intent(context.getApplicationContext(), UserInformationActivity.class);
-                context.startActivity(myIntent);
-            }
-        });
+ //       view.setOnClickListener(new View.OnClickListener() {
+ //           @Override
+ //           public void onClick(View v) {
+ //               Intent myIntent = new Intent(context.getApplicationContext(), UserInformationActivity.class);
+ //               context.startActivity(myIntent);
+  //          }
+  //      });
 
         return new InProgressReservationViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull InProgressReservationViewHolder inProgressReservationViewHolder, int position) {
+        //TextView textViewOrderIdReservation = inProgressReservationViewHolder.textViewOrderIdReservation;
+
         TextView textViewOrderId = inProgressReservationViewHolder.textViewReservationId;
         TextView textViewRemainingTime = inProgressReservationViewHolder.textViewRemainingTime;
         TextView textViewTotalIncome = inProgressReservationViewHolder.textViewTotalIncome;
@@ -96,6 +104,28 @@ public class InProgressReservationsListAdapter extends RecyclerView.Adapter<InPr
                 finishedDataSet.add(tmpRM);
             }
         });
+/*
+        inProgressReservationViewHolder.itemView.setOnClickListener(v -> {
+            DetailsFragment detailsFragment = DetailsFragment.newInstance(tmpRM);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                detailsFragment.setSharedElementEnterTransition(new DetailsTransition());
+                detailsFragment.setEnterTransition(new Fade());
+                detailsFragment.setExitTransition(new Fade());
+                detailsFragment.setSharedElementReturnTransition(new DetailsTransition());
+            }
+
+            ViewCompat.setTransitionName(textViewOrderIdReservation,"lessDetails");
+            fragmentActivity.getSupportFragmentManager()
+                    .beginTransaction()
+                    .addSharedElement(textViewOrderIdReservation,
+                            "seeDetails")
+                    .replace(R.id.frame_container_reservations, detailsFragment)
+                    .addToBackStack(null)
+                    .commit();
+        });
+
+*/
     }
 
     @Override
